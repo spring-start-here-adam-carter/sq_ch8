ae8185753b9f069cdd63fc076b50449822e554a7
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 15:25:51 -0500

    Add color request parameter to home endpoint

--- src/main/java/com/example/sq_ch8/controllers/MainController.java
+++ src/main/java/com/example/sq_ch8/controllers/MainController.java
@@ -3,14 +3,17 @@ package com.example.sq_ch8.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(Model page) {
+    public String home(
+            Model page,
+            @RequestParam String color) {
         page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+        page.addAttribute("color", color);
         return "home.html";
     }
 }
