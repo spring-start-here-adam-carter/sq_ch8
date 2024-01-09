4fa7f3d8a181df88b3e58e5c748ba3760625d227
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 16:13:17 -0500

    Update /products POST endpoint by using the model as a parameter

--- src/main/java/com/example/sq_ch8/controllers/ProductsController.java
+++ src/main/java/com/example/sq_ch8/controllers/ProductsController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductsController {
@@ -29,13 +27,9 @@ public class ProductsController {
 
     @PostMapping(path = "/products")
     public String addProduct(
-            @RequestParam String name,
-            @RequestParam double price,
+            Product p,
             Model model
     ) {
-        Product p = new Product();
-        p.setName(name);
-        p.setPrice(price);
         productService.addProduct(p);
 
         var products = productService.findAll();
