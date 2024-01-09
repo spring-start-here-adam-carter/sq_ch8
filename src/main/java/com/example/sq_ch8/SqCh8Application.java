851bdd6951fbfb960ac022eb43e17f83f2c0c94d
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 13:48:48 -0500

    Initial commit

---
+++ src/main/java/com/example/sq_ch8/SqCh8Application.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sq_ch8/SqCh8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
