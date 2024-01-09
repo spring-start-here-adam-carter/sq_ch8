74a4b0cb123e166e8aa2e2d14d8e4d9b798480d3
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-09 15:37:04 -0500

    Use a path variable to send the color to the home endpoint

--- src/main/java/com/example/sq_ch8/controllers/MainController.java
+++ src/main/java/com/example/sq_ch8/controllers/MainController.java
@@ -2,18 +2,17 @@ package com.example.sq_ch8.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home/{color}")
     public String home(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String color,
+            @PathVariable String color,
             Model page) {
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Katy");
         page.addAttribute("color", color);
         return "home.html";
     }
